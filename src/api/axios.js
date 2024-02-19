cc28b7ab16929cfd47d3027b72ac22b4f098a1b5
funmeen <irfanmuhymeen@gmail.com>
2024-02-19 12:11:55 +0800

    Update baseURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://ec2-18-141-41-29.ap-southeast-1.compute.amazonaws.com/api/user'
+    baseURL: 'http://18.141.41.29:4000/api/user/'
 });
