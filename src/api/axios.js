2e769f942daba6de04603695cfe75730c069eb81
funmeen <irfanmuhymeen@gmail.com>
2024-02-29 16:26:34 +0800

    update headers ii

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://small-badgers-sell.loca.lt/',
+    baseURL: 'https://thick-places-hope.loca.lt/',
     headers: {
         'bypass-tunnel-reminder': 'true' // Set any value for the header
       }
