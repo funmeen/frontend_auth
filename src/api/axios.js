b8005ae520539c5716d6c5c1a100bd7d7f915ae7
funmeen <irfanmuhymeen@gmail.com>
2024-02-29 16:23:58 +0800

    update headers

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://small-badgers-sell.loca.lt/'
+    baseURL: 'https://small-badgers-sell.loca.lt/',
+    headers: {
+        'bypass-tunnel-reminder': 'true' // Set any value for the header
+      }
 });
