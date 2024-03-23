1509039bb824df2971e6a8656016b3b36ce2bf15
funmeen <irfanmuhymeen@gmail.com>
2024-03-23 13:57:59 +0800

    update baseURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://fresh-beds-invent.loca.lt/',
-    headers: {
-        'bypass-tunnel-reminder': 'true' // Set any value for the header
-      }
+    baseURL: 'https://back.norwintech.solutions/',
 });
